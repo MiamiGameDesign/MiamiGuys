d7aee6ff1420eaeb6dee8095384a57a3b4d5d6c7
MiamiGameDesign <gamedesignclub@miamioh.edu>
2023-03-07 19:28:08 -0500

    Added Title Screen

---
+++ Assets/Scripts/TitleScreenStart.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenStart : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainLevel");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
